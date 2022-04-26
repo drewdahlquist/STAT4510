455cdd21e9eedac5c39778b5841796a3b8f7a292
Drew Dahlquist <drewdahlquistdev@gmail.com>
2022-04-25 21:34:44 -0500

    initial progress

---
+++ project/project.R
@@ -0,0 +1,23 @@
+library(dplyr)
+
+# data
+beans = read.csv("/Users/drew/Desktop/OneDrive - University of Missouri/sp 22/STAT 4510/project/beans.csv")
+beans$Class = as.factor(beans$Class)
+
+# train/testing split
+set.seed(1)
+train = sample(1:nrow(beans), 0.7*nrow(beans))
+beans.train = beans[train,]
+beans.test = beans[-train,]
+
+# tree's
+library(tree)
+tree.fit = tree(Class ~ ., beans, subset=train)
+plot(cv.tree(tree.fit), type="b")
+
+pred = predict(tree.fit, newdata = beans[-train,], type="class")
+table = table(beans.test$Class, yhat)
+
+# some other method? svm?
+
+# maybe one more? 
